4a64fe4e47711e1e09105770c57b84f10cd9f37d
郭林凯 <895947135@qq.com>
2020-07-19 20:10:10 +0800

    数据类型练习

--- dataType.ts
+++ dataType.ts
@@ -7,7 +7,14 @@
                 数组
                 元组
         enum    枚举
+        void    多用于表示方法返回值的类型，表示方法没有返回值
+        null    表示对象值缺失
+        undefined   用于初始化变量为一个未定义的值
+        never   是其他类型（null 和 undefined）的子类型，代表从不会出现的值
+
+        typescript 和 javascript 没有整数类型
  */
+
 
 // any
 let anyway: any = '任何类型';
@@ -42,6 +49,20 @@ console.log('color3:', color3);
 function fn (): void {
     console.log('void')
 }
-let rn:void = fn();
+let rn: void = fn();
 console.log('rn:', rn);
+
+// null 和 undefined 可以赋值给其他类型
+let num2: number = undefined;
+console.log('num2:', num2);
+// 空校验（strictNullChecks）下，null 和 undefined 只能赋给 void 或本身对应的类型
+let vo: void = undefined;
+let nul: null = null;
+let und: undefined = undefined;
+console.log('vo:', vo);
+console.log('nul:', nul);
+console.log('und:', und);
+// 想要可以赋值 undefined 可以使用 |
+let num3: number | undefined = undefined;
+
 
