d306540992e9bd8ee18468dac0c23f3bc4080b82
glk-asdf <895947135@qq.com>
2020-07-22 14:08:25 +0800

    添加 never

--- dataType.ts
+++ dataType.ts
@@ -10,7 +10,7 @@
         void    多用于表示方法返回值的类型，表示方法没有返回值
         null    表示对象值缺失
         undefined   用于初始化变量为一个未定义的值
-        never   是其他类型（null 和 undefined）的子类型，代表从不会出现的值
+        never   是其他类型（包括 null 和 undefined）的子类型，代表从不会出现的值
 
         typescript 和 javascript 没有整数类型
  */
@@ -38,6 +38,7 @@ let arr2: [number, boolean] = [1, true];
 
 // 枚举
 enum Color {Red, Green, Blue}
+
 let color1: Color = Color.Red;
 console.log('color1:', color1);
 let color2: Color = Color.Green;
@@ -46,15 +47,17 @@ let color3: Color = Color.Blue;
 console.log('color3:', color3);
 
 // void
-function fn (): void {
+function fn(): void {
     console.log('void')
 }
+
 let rn: void = fn();
 console.log('rn:', rn);
 
 // null 和 undefined 可以赋值给其他类型
 let num2: number = undefined;
 console.log('num2:', num2);
+
 // 空校验（strictNullChecks）下，null 和 undefined 只能赋给 void 或本身对应的类型
 let vo: void = undefined;
 let nul: null = null;
@@ -64,5 +67,24 @@ console.log('nul:', nul);
 console.log('und:', und);
 // 想要可以赋值 undefined 可以使用 |
 let num3: number | undefined = undefined;
+
+// never 是其他类型（包括 null 和 undefined）的子类型，代表从不会出现的值
+// 意味着 never 只能被 never 类型所赋值
+// 在函数中通常表现为抛出异常或无法执行到终点（例如无限循环）
+let nev: never = (() => {
+    throw new Error('exception')
+})();
+let num4: number = (() => {
+    throw new Error('exception')
+})();
+
+function error(): never {
+    throw new Error('exception')
+}
+
+function loop(): never {
+    while (true) {
+    }
+}
 
 
