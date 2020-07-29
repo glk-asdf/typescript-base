b9c989d933ac07dfc0ef7dbd158035a41a54608d
glk-asdf <895947135@qq.com>
2020-07-29 15:52:57 +0800

    123

---
+++ array.ts
@@ -0,0 +1,7 @@
+/*
+    array 数组对象是使用单独的变量名来存储一系列值
+ */
+
+// 多维数组
+let multi: number[][] = [[1, 2], [3, 4]];
+console.log('multi:', multi);
---
+++ unionTypes.ts
@@ -0,0 +1,12 @@
+/*
+    联合类型
+    通过管道 | 设置多种类型
+        type1|type2|type3
+        type1 | type2 | type3
+ */
+
+let a: number | string;
+a = 1;
+console.log(a);
+a = 'asd';
+console.log(a);
