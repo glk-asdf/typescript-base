48c56ff5c41c7e7bca3fb0f4dec2869596bebafd
guolk <guolk@hqenv.com>
2020-10-26 16:30:55 +0800

    .gitignore

--- baseGrammar.ts
+++ baseGrammar.ts
@@ -1,20 +1,23 @@
 /*
     基础语法
-        声明变量时添加添加类型
+        声明变量时添加类型
+        方法添加参数类型
         方法添加返回值类型
  */
 
 // hello world
 let hello: string = 'hello world';
-console.log(hello);
+console.log('hello:', hello);
+let num : number | undefined = undefined;
+console.log('num:', num);
 
 /**
  * class Site 一个类
  * function name 方法
  */
 class Site {
-    name(): void {
-        console.log('glk')
+    name(str: string = 'glk'): void {
+        console.log(str)
     }
 }
 // 实例化
--- dataType.ts
+++ dataType.ts
@@ -38,13 +38,21 @@ let arr2: [number, boolean] = [1, true];
 
 // 枚举
 enum Color {Red, Green, Blue}
+console.log('color:', Color);
 
 let color1: Color = Color.Red;
 console.log('color1:', color1);
+console.log('Color[color1]:', Color[color1]);
 let color2: Color = Color.Green;
 console.log('color2:', color2);
+console.log('Color[color2]:', Color[color2]);
 let color3: Color = Color.Blue;
 console.log('color3:', color3);
+console.log('Color[color3]:', Color[color3]);
+// 不能使用未枚举的数据
+// let color4: Color = Color.Yello;
+// console.log('color4:', color4);
+// console.log('Color[color4]:', Color[color4]);
 
 // void
 function fn(): void {
@@ -55,7 +63,7 @@ let rn: void = fn();
 console.log('rn:', rn);
 
 // null 和 undefined 可以赋值给其他类型
-let num2: number = undefined;
+let num2: number | undefined = undefined;
 console.log('num2:', num2);
 
 // 空校验（strictNullChecks）下，null 和 undefined 只能赋给 void 或本身对应的类型
